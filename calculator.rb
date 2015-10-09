72381d68876dbe8cfe7f9c4dbefc2316715618a9
Matias Fernandez <matiasfmolinari@gmail.com>
2015-10-09 00:20:34 -0400

    Clean program comments and spacing

--- calculator.rb
+++ calculator.rb
@@ -1,47 +1,3 @@
-=begin
-What are you trying to build?
-  A calculator that can add, subtract, multiply, and divide two
-  inputs.
-
-What are the logical steps to accomplish your goal?
-
-  Input
-    - Ask user for first_number. 
-    - If first_number != integer, raise error and repeat.
-    - Ask user for second_number.
-    - If first_number != integer, raise error and repeat.
-    - Ask user for a symbol (+, -, *, or /).
-    - If first_number != (+, -, *, or /), raise error and repeat.
-  
-  Calculation
-    - If the symbol is '+', add the two numbers.
-    - Elsif the symbol is '-', subtract the two numbers.
-    - Elsif the symbol is '*', multiply the two numbers.
-    - Else the symbol is '/', so divide the two numbers.
-
-  Output
-    - Print the answer.
-
-ask
-get if not number: error
-print
-ask
-get
-print
-
-to_arr
-each
-  add number to "equals" if first in arr
-  case
-    number
-    symbol
-
-1, 2, 3, 4, 5
-   *, /, %, -
-
-=end
-
-
 def not_a_number?(number)
   !(number =~ /\A[-+]?[0-9]*\.?[0-9]+\Z/)
 end
@@ -51,7 +7,7 @@ def not_an_operator?(operator)
 end
 
 def ask_for_input
-  equation  = ""
+  equation = ""
 
   loop do
     print "Enter number: "
@@ -86,6 +42,7 @@ end
 def calculate(equation)
   "= #{eval(equation)}"
 end
+
 
 puts calculate(ask_for_input)
 puts
